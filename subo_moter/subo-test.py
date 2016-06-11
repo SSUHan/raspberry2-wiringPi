b100264b57289f87f0b80322f015b168a1ba38d1
BrianLee <ljs93kr@gmail.com>
2016-06-11 11:44:48 +0000

    subo angle test

--- subo_moter/subo-test.py
+++ subo_moter/subo-test.py
@@ -9,12 +9,34 @@ p = GPIO.PWM(pin, 50)
 p.start(7.5)
 try:
 	while True:
-		p.ChangeDutyCycle(7.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(12.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(2.5)
-		time.sleep(1)
+		p.ChangeDutyCycle(1)
+		print "angle : 1"
+		time.sleep(2)
+#		p.ChangeDutyCycle(2)
+#		print "angle : 2"
+#		time.sleep(2)
+#		p.ChangeDutyCycle(3)
+#		print "angle : 3"
+#		time.sleep(2)
+#		p.ChangeDutyCycle(4)
+#		print "angle : 4"
+#		time.sleep(2)
+#		p.ChangeDutyCycle(5)
+#		print "angle : 5"
+#		time.sleep(2)
+#		p.ChangeDutyCycle(6)
+#		print "angle : 6"
+#		time.sleep(2)
+#		p.ChangeDutyCycle(7)
+#		print "angle : 7"
+#		time.sleep(2)
+		p.ChangeDutyCycle(8)
+		print "angle : 8"
+		time.sleep(2)
+#p.ChangeDutyCycle(9)
+#		print "angle : 9"
+#		time.sleep(2)
+		
 except KeyboardInterrupt:
 	p.stop()
 GPIO.cleanup()
